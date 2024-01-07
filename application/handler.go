5f88d5268e5aa81d44763090fe8651274c2dbac6
safon <safonchik.jn@gmail.com>
2024-01-07 14:54:02 +0200

    now returns whole url when shortening

--- application/handler.go
+++ application/handler.go
@@ -42,6 +42,7 @@ func (l *Link) MakeShort(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	link.Short = "localhost:3001/" + link.Short
 	res, err := json.Marshal(link)
 	if err != nil {
 		fmt.Println("failed to marshal:", err)
